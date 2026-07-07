63d05f49380591cd5bb2515e31a02b3e2bfa0ebb
ImtiazAhmed01 <imtiazahm21@gmail.com>
2026-07-08 00:54:47 +0600

    initial project running check

---
+++ app/components/InsightFlow.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function InsightFlow() {
+    return (
+        <div>
+            <h1> this is InsightFlow section</h1>
+        </div>
+    );
+};
---
+++ app/components/Signature3D.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Singature3D() {
+    return (
+        <div>
+            <h1> this is Singature3D section</h1>
+        </div>
+    );
+};
